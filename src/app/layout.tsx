d34e76fc17147a7f488a9e149df0d576071de0cf
dostrowski3 <dawid.ostrowski@intercity.pl>
2025-08-13 23:42:05 +0200

    zmiana meta danych dla obrazu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,21 +13,23 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-    metadataBase: new URL('http://localhost:3000'),
+    metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? 'https://aimora.pl'),
     title: 'Aimora – Interaktywne Cele Strzeleckie z Aplikacją',
     description: 'Rewolucyjny system inteligentnych detektorów trafień do treningu strzeleckiego',
-    keywords: ['strzelanie', 'trening', 'cele', 'smart', 'technologia', 'aimora', 'ASG', 'wiatrówka'],
+    keywords: ['strzelanie', 'trening', 'cele', 'militaria', 'technologia', 'aimora', 'ASG', 'wiatrówka', 'airsoft', 'strzelectwo', 'broń', 'cele strzeleckie', 'gong strzelecki', 'tarcze strzeleckie'],
     authors: [{ name: 'Aimora' }],
     robots: 'index, follow',
     openGraph: {
-        title: 'Aimora – Interaktywne Cele Strzeleckie z Aplikacją',
+        title: 'Aimora – Interaktywne Cele Strzeleckie z aplikacją mobilną',
         description: 'Rewolucyjny system inteligentnych detektorów trafień do treningu strzeleckiego',
         type: 'website',
+        images: ['/images/what_is_it_mobile.png'],
     },
     twitter: {
         card: 'summary_large_image',
-        title: 'Aimora – Interaktywne Cele Strzeleckie z Aplikacją',
+        title: 'Aimora – Interaktywne Cele Strzeleckie z aplikacją mobilną',
         description: 'Rewolucyjny system inteligentnych detektorów trafień do treningu strzeleckiego',
+        images: ['/images/what_is_it_mobile.png'],
     },
 }
 
